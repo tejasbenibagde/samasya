12cf90805bee81b5c01cb3abb766c9f92ffcf652
tejasbenibagde <124677750+tejasbenibagde@users.noreply.github.com>
2024-11-24 15:42:00 +0530

    Restarting the work

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-// Export all modules here
 export * from './core/arithmetic';
-// export * from './core/constants';
-// export * from './core/trigonometry';
-// export * from './core/utils';
-
-// Add more exports as you develop other modules
